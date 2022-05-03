d1c57384a7598bbf486313ebb5108ff1ef94f3b3
hossein <hossein838485@gmail.com>
2022-05-03 20:47:05 +0430

    create and end css and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+let heighTasks = document.querySelector(".tasks");
+let heightProjects = document.querySelector(".projects");
+let heightInput = document.querySelector('.task-bar')
+let screenhight = window.innerHeight; 
+heighTasks.style.height = `${screenhight}px `;
+heightProjects.style.height = `${screenhight}px`
+
+
+
