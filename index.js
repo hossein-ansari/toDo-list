9e4ee74d38d4a913d19e6003d4c894e7ba734a5d
hossein <hossein838485@gmail.com>
2022-05-04 13:49:40 +0430

    end front end (index.css , index.html , index.js)

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-let heighTasks = document.querySelector(".tasks");
-let heightProjects = document.querySelector(".projects");
-let heightInput = document.querySelector('.task-bar')
-let screenhight = window.innerHeight; 
-heighTasks.style.height = `${screenhight}px `;
-heightProjects.style.height = `${screenhight}px`
+let $ = document;
+let heighTasks = $.querySelector(".tasks");
+let heightProjects = $.querySelector(".projects");
+let heightInput = $.querySelector(".task-flex");
+let projectComponent = $.querySelector(".project-component");
+let projectsLi = $.querySelectorAll(".projects-li");
+let projectsIcon = $.querySelector(".projects-icon");
 
+// height screen
+let screenHight = window.innerHeight;
+heighTasks.style.height = `${screenHight}px `;
+heightProjects.style.height = `${screenHight}px`;
+heightInput.style.height = `${screenHight - 135}px`;
 
+// toggle project component
 
+projectComponent.addEventListener("click", (e) => {
+  if (projectsIcon.classList.contains("fa-angle-down")) {
+    projectsIcon.classList.remove("fa-angle-down");
+    projectsIcon.classList.add("fa-angle-right");
+  } else {
+    projectsIcon.classList.add("fa-angle-down");
+    projectsIcon.classList.remove("fa-angle-right");
+  }
+  projectsLi.forEach((e) => {
+    e.classList.toggle("remove");
+  });
+});
