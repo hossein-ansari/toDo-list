a305b541c5b1f6af8696410fb5c1481b2bd2e2ed
hossein <hossein838485@gmail.com>
2022-05-07 23:14:06 +0430

    add task and save in local

--- app.js
+++ app.js
@@ -2,23 +2,44 @@
 let input = document.querySelector(".input");
 let addTask = document.querySelector(".add-task");
 let ulTask = document.querySelector(".task-box");
- let toDoArray = []
-function taskAdder() {
-      ulTask.insertAdjacentHTML(
-    'afterend',
-    '<li class="li-tasks position"><div>'+input.value+'</div><div class="li-option"><i class="li-btn fa-solid fa-check "></i><i class="fa-solid fa-trash li-btn"></i></div></li>'
-    );
-    let newToDo = {
-      id : toDoArray.length + 1,
-      title : input.value,
-      complete : false,
-    }
-    toDoArray.push(newToDo)
-    localToDo(toDoArray)
+let toDoArray = [];
+function taskAdder(lis) {
+  let newToDo = {
+    id: toDoArray.length + 1,
+    title: input.value,
+    complete: false,
+  };
+  toDoArray.push(newToDo);
+  localToDo(toDoArray)
+  addgenerator(toDoArray)  
+  input.value = "";
 }
 function localToDo(todoElem) {
-    localStorage.setItem('todos', JSON.stringify(todoElem))
+  localStorage.setItem("todos", JSON.stringify(todoElem));
 }
-
+function addgenerator(todoElem) {
+  ulTask.innerHTML = '' 
+  toDoArray.forEach(todoElem => {
+    
+    ulTask.insertAdjacentHTML(
+      "beforeend",
+      '<li class="li-tasks position"><div>' +
+      todoElem.title +
+      '</div><div class="li-option"><i class="li-btn fa-solid fa-check "></i><i class="fa-solid fa-trash li-btn"></i></div></li>'
+      );
+      
+    });
+  }
+function reUploadToDo(e) {
+  let localback = JSON.parse(localStorage.getItem("todos"));
+  if (localback) {
+    toDoArray = localback
+  }
+  else{
+    toDoArray = []
+  }
+  console.log(localback);
+  addgenerator(toDoArray)
+}
+window.addEventListener("DOMContentLoaded", reUploadToDo);
 addTask.addEventListener("click", taskAdder);
-
