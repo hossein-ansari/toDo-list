e09e9dd451c730d70a89d69e91a71b69d29c88c5
hossein <hossein838485@gmail.com>
2022-05-07 18:42:18 +0430

    add localstorage

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 let input = document.querySelector(".input");
 let addTask = document.querySelector(".add-task");
 let ulTask = document.querySelector(".task-box");
-let count = parseFloat(window.innerHeight / 11) 
-console.log(count);
+ let toDoArray = []
 function taskAdder() {
       ulTask.insertAdjacentHTML(
     'afterend',
     '<li class="li-tasks position"><div>'+input.value+'</div><div class="li-option"><i class="li-btn fa-solid fa-check "></i><i class="fa-solid fa-trash li-btn"></i></div></li>'
     );
+    let newToDo = {
+      id : toDoArray.length + 1,
+      title : input.value,
+      complete : false,
+    }
+    toDoArray.push(newToDo)
+    localToDo(toDoArray)
 }
+function localToDo(todoElem) {
+    localStorage.setItem('todos', JSON.stringify(todoElem))
+}
+
 addTask.addEventListener("click", taskAdder);
+
