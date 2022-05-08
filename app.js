28fadad35a45ea11f278b9248aa8dd0db45f040f
hossein <hossein838485@gmail.com>
2022-05-08 21:18:34 +0430

    add delet btn in local and dom

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 let input = document.querySelector(".input");
 let addTask = document.querySelector(".add-task");
 let ulTask = document.querySelector(".task-box");
+let trashBtn = document.querySelector(".fa-trash");
+let li = document.querySelector(".position");
+
 let toDoArray = [];
 function taskAdder(lis) {
   let newToDo = {
@@ -10,36 +13,49 @@ function taskAdder(lis) {
     complete: false,
   };
   toDoArray.push(newToDo);
-  localToDo(toDoArray)
-  addgenerator(toDoArray)  
+  localToDo(toDoArray);
+  addgenerator(toDoArray);
   input.value = "";
 }
 function localToDo(todoElem) {
   localStorage.setItem("todos", JSON.stringify(todoElem));
 }
 function addgenerator(todoElem) {
-  ulTask.innerHTML = '' 
-  toDoArray.forEach(todoElem => {
-    
+  ulTask.innerHTML = "";
+  toDoArray.forEach((todoElem) => {
     ulTask.insertAdjacentHTML(
       "beforeend",
       '<li class="li-tasks position"><div>' +
-      todoElem.title +
-      '</div><div class="li-option"><i class="li-btn fa-solid fa-check "></i><i class="fa-solid fa-trash li-btn"></i></div></li>'
-      );
-      
-    });
-  }
+        todoElem.title +
+        '</div><div class="li-option"><i class="li-btn fa-solid fa-check "></i><i onclick=" clearHandeler('+todoElem.id+') " class="fa-solid fa-trash li-btn"></i></div></li>'
+    );
+  });
+}
 function reUploadToDo(e) {
   let localback = JSON.parse(localStorage.getItem("todos"));
   if (localback) {
-    toDoArray = localback
+    toDoArray = localback;
+  } else {
+    toDoArray = [];
   }
-  else{
-    toDoArray = []
-  }
-  console.log(localback);
-  addgenerator(toDoArray)
+
+  addgenerator(toDoArray);
 }
 window.addEventListener("DOMContentLoaded", reUploadToDo);
 addTask.addEventListener("click", taskAdder);
+//  clear btn
+
+function clearHandeler(id) {
+  let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+  toDoArray = localStorageTodos
+
+ let mainTodoIndex = toDoArray.findIndex(function (todo) {
+     return todo.id === id
+ })
+
+ toDoArray.splice(mainTodoIndex , 1)
+ localToDo(toDoArray)
+ addgenerator(toDoArray)
+}
+
+
